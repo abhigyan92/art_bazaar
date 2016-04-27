0249dd31ee8278061d1c70f475d30e1fca844bd5
Abhigyan <abhigyan111190@gmail.com>
2016-04-27 13:09:11 +0530

    migration deleted

--- db/migrate/20160427043802_fix_column_name.rb
+++
@@ -1,6 +0,0 @@
-class FixColumnName < ActiveRecord::Migration
-  def change
-  	rename_column :transaction_details, :order_id, :transaction_id
-  	rename_column :transactions, :cart_id, :order_id
-  end
-end
