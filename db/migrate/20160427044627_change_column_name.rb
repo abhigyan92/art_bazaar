51cbcc2c8249bf689b0b176a0dd8e846e6d96078
Abhigyan <abhigyan111190@gmail.com>
2016-04-27 13:42:53 +0530

    error resolved-2

--- db/migrate/20160427044627_change_column_name.rb
+++ db/migrate/20160427044627_change_column_name.rb
@@ -2,5 +2,7 @@ class ChangeColumnName < ActiveRecord::Migration
   def change
   	rename_column :transaction_details, :order_id, :transaction_id
   	rename_column :transactions, :cart_id, :order_id
+  	remove_foreign_key :orders, :order_statuses
+
   end
 end
