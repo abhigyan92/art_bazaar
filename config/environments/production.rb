61af4db9c0de5af352df3c38f5bef2248072f7a3
Abhigyan <abhigyan111190@gmail.com>
2016-04-27 14:31:03 +0530

    fixing mailer issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,24 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  onfig.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  host = 'art-bazaar.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default :charset => "utf-8"
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "gd1ar2@gmail.com",
+  password: "1290819100084",
+  domain: 'gmail.com'
+}
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
