b22116782e0ed9a2b0b0f9df7cb2f231d8fc9d14
Abhigyan <abhigyan111190@gmail.com>
2016-04-28 15:46:48 +0530

    Admin section frontend

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ActiveRecord::Base
     default_scope { where(active: true) }
 	mount_uploader :image,PictureUploader
 	validates :description, presence: true, length: { maximum: 140 }
-	validates :name,presence:true,length:{maximum:50}
-	validates :price,presence:true
+	validates :name,presence:true,length:{maximum:50},uniqueness: true
+	validates :price,presence:true,numericality: { only_integer: true,
+		greater_than_or_equal_to:0,less_than_or_equal_to:1000000}
+	validates :image,presence:true
 end
--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -4,5 +4,8 @@ class LoginControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "title", "ArtBazaar|Login"
   end
+
+
 end
